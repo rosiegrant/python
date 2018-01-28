63cf0360aaec6a605db6d7cc885b9fdcef5d807d
Rose <rosellen.grant@gmail.com>
2018-01-27 20:21:39 -0500

    initial test set up

---
+++ scraper1.py
@@ -0,0 +1,19 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+
+def main ():
+	url = 'http://www.showmeboone.com/sheriff/JailResidents/JailResidents.asp'
+	response = requests.get(url)
+	html = response.content
+	print html
+
+	soup = BeautifulSoup(html)
+	print soup.prettify()
+
+	table = soup.find('tbody', attrs={'class': 'stripe'})
+	for row in table.findAll('tr'):
+		for cell in row.findAll('td'):
+			print row.prettify()
+
+
+if __name__ == "__main__": main()
