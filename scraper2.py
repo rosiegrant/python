f329b4bf46b4de2ed337094cdda11f702da7aa50
Rosellen Grant <rosellen.grant@gmail.com>
2018-01-28 00:01:42 -0500

    full run, checking for locs that don't match pattern

--- scraper2.py
+++ scraper2.py
@@ -12,8 +12,7 @@ def main():
     writer.writerow(
         ["CenterID", "CenterURL", "Subpage Name", "Subpage URL"])  # first row, of headers
 
-    # command line argument should be file with list of URLs. iterate through
-    # URLs
+    # iterate through URLs, from file that's taken in via the command line
     infile = open(sys.argv[1])
     infile.readline()  # throw out the first line with headers
 
@@ -22,6 +21,8 @@ def main():
         row = line.split(',')
         centerID = row[0]
         pageURL = row[1].replace("\n", "")
+
+        print("processing store ", centerID)
 
         all_rows = soupStuff(centerID, pageURL)  # do the beautiful soup things
         writer.writerows(all_rows)  # write data to outfile
@@ -34,28 +35,31 @@ def soupStuff(centerID, pageURL):
     response = requests.get(pageURL)
     html = response.content
     soup = BeautifulSoup(html, "html.parser")
-    print(soup.prettify())
-    footer = soup.findAll("div", {"class": "footerColumn"})[
-        1]
-    # print(footer)
+    # print(soup.prettify())
+    footerA = soup.findAll("div", {"class": "footerColumn"})
+    if (len(footerA) > 3):
+        print("Number of footer elements higher than expected for store ",
+              centerID)  # this store doesn't match the pattern
+    footer = footerA[1]
 
     prod_services = footer.findAll('h4')[0]
-    # print(prodAndServ)
 
     all_rows = []
     first_row = []
+    first_row.append(centerID), first_row.append(pageURL)
     first_row.append(prod_services.a.contents[0]), first_row.append(
         prod_services.a.get('href'))
     all_rows.append(first_row)  # well this is dumb
 
     for listItem in footer.findAll('li'):
         this_row = []
-        this_row.append(listItem.a.contents[0])
-        this_row.append(listItem.a.get('href'))
+        this_row.append(centerID), this_row.append(pageURL)
+        this_row.append(listItem.a.contents[0])  # subpage name
+        this_row.append(listItem.a.get('href'))  # subpage URL
         all_rows.append(this_row)
 
     return all_rows
 
 
 if __name__ == "__main__":
-    main()
+    main()  # do it
