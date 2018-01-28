7ae774651dd36cccceda2c393b625929a6f2e04d
Rosellen Grant <rosellen.grant@gmail.com>
2018-01-27 23:22:53 -0500

    pulled soupstuff out of main

--- scraper2.py
+++ scraper2.py
@@ -5,19 +5,36 @@ import sys
 
 
 def main():
-    url = 'http://theupsstorelocal.com/5635'
 
-    for line in open(sys.argv[1]):
-        row = line.split(',')  # returns a list ["1","50","60"]
-        CenterID = row[0]
-        pageURL = row[1]
+    # prepare output file
+    outfile = open("./upslinks.csv", "w")
+    writer = csv.writer(outfile)
+    writer.writerow(
+        ["CenterID", "CenterURL", "Subpage Name", "Subpage URL"])  # first row, of headers
+
+    # command line argument should be file with list of URLs. iterate through
+    # URLs
+    infile = open(sys.argv[1])
+    infile.readline()  # throw out the first line with headers
+
+    for line in infile:
+
+        row = line.split(',')
+        centerID = row[0]
+        pageURL = row[1].replace("\n", "")
+
+        all_rows = soupStuff(centerID, pageURL)  # do the beautiful soup things
+        writer.writerows(all_rows)  # write data to outfile
+
+    outfile.close()
+
+
+def soupStuff(centerID, pageURL):
 
     response = requests.get(pageURL)
     html = response.content
-    # print(html)
-
     soup = BeautifulSoup(html, "html.parser")
-    # print(soup.prettify())
+    print(soup.prettify())
     footer = soup.findAll("div", {"class": "footerColumn"})[
         1]
     # print(footer)
@@ -35,14 +52,9 @@ def main():
         this_row = []
         this_row.append(listItem.a.contents[0])
         this_row.append(listItem.a.get('href'))
-        # print("an item", listItem.a.get('href'))
-        # print("an item", listItem.a.contents[0])
         all_rows.append(this_row)
 
-    outfile = open("./upslinks.csv", "w")
-    writer = csv.writer(outfile)
-    writer.writerow(["CenterID", "CenterURL", "Subpage Name", "Subpage URL"])
-    writer.writerows(all_rows)
+    return all_rows
 
 
 if __name__ == "__main__":
