57ae091fb58d9490cffd327d638ec38c935529f9
Rose <rosellen.grant@gmail.com>
2018-10-10 01:50:24 -0400

    error checking, initial runs

--- slscrape.py
+++ slscrape.py
@@ -10,27 +10,27 @@ def main():
     outfile = open("./slscrape.csv", "w")
     writer = csv.writer(outfile)
     writer.writerow(
-        ["CenterID", "CenterURL", "Subpage Name", "Subpage URL"])  # first row, of headers
+        ["Entity ID", "Client URL", "Business Name", "Phone","Rate","Services", "About Main Title", "About Header 1", 
+        	"About Description 1", "About Header 2", "About Description 2", "About Header 3", "About Description 3", 
+        	"About Header 4", "About Description 4", "About Header 5", "About Description 5"])  # Add headers to output file
 
-    # iterate through URLs, from file that's taken in via the command line
+    # iterate through URLs, from file that's taken in via the command line, first argument
     infile = open(sys.argv[1])
     infile.readline()  # throw out the first line with headers
 
     for line in infile:
 
         row = line.split(',')
-        # print(row)
         entityId = row[0]
         pageURL = row[2].replace("\n", "")
-        # print("pageURL", pageURL)
         print("processing store ", entityId)
+        writer.writerow(soupStuff(entityId, pageURL))
 
-       	# soupStuff(entityId, pageURL)  # do the beautiful soup things
 
        # allRows = soupStuff(entityId, pageURL)  # do the beautiful soup things
        # writer.writerows(allRows)  # write data to outfile
 
-    soupStuff("1234", "http://www.seniorlifestyle.com/property/sheridan-mason/")
+    # writer.writerow(soupStuff("1234", "http://www.seniorlifestyle.com/property/morningside-house-leesburg/"))
     outfile.close()
 
 
@@ -39,53 +39,60 @@ def soupStuff(entityId, pageURL):
     response = requests.get(pageURL)
     html = response.content
     soup = BeautifulSoup(html, "html.parser")
-    # print(soup.prettify())
 
-    # get business name and phone number
-    span9 = soup.findAll("div", {"class": "span9"}) #find all divs whose class is span9
-    if (len(span9) == 0):
-        print("Can't find any span9 column classes",
-              entityId)  # this store doesn't match the pattern
+    thisRow = [] # Start assembling the row that will be added to the output file
+    thisRow.append(entityId), thisRow.append(pageURL)
+
+    # Get Business Name and Phone Number
+    span9 = soup.findAll("div", {"class": "span9"}) # find all divs who have the span9 class
+    if (len(span9) == 0 or span9[0].find('h2') is None):
+        print("Error for ", entityId)  
+        thisRow.append("Error found, sorry!") # This store doesn't match the pattern, abort and return an empty row with an error
+        return thisRow
+
     nap = span9[0]
-    name = nap.find('h2').text
-    # print(name)
-    phone = nap.find(itemprop="telephone").text
-    # print(phone)
+    name = nap.find('h2') # business name
+    if name: thisRow.append(name.text) 
+    else: thisRow.append(" ") # no name found
 
-    # get rate
-    rate = soup.find(itemprop="priceRange").text
-    # print(rate)
+    phone = nap.find(itemprop="telephone") # phone number
+    if phone: thisRow.append(phone.text) 
+    else: thisRow.append(" ") # no phone found
 
-    # get services 
-    # for service in soup.find(text='providing').parent.find_next_sibling():
-    # 	print(service.text)
+    # Get Rate
+    rate = soup.find(itemprop="priceRange")
+    if rate: thisRow.append(rate.text)
+    else: thisRow.append(" ") # no rate found
 
-    # About main title
+    # Get Services, store in a pipe delimited cell 
+    # TODO: there's always a trailing pipe, fix that
+    services = ""
+    for service in soup.find(text='providing').parent.find_next_sibling():
+    	services = services + service.text + " | " 
+    thisRow.append(services)
+
+    # Get About Main Title
     about = soup.find(itemtype="http://schema.org/Organization", id="about")
     mainTitle = about.h1.text
-    # print(mainTitle)
+    thisRow.append(mainTitle)
 
+
+    # Content
     h4s = about.findAll('h4')
-    ps = about.findAll('p')
-
-    h4length = len(h4s)
-    pslength = len(ps)
-
-    # sib = about.h4.next_sibling;
-    # while sib == 'p':
-    # 	print(sib)
-    # 	sib = sib.next_sibling;
     for header in h4s:
     	sib = header.find_next_sibling()
-    	print(header)
+    	thisRow.append(header.text) # About Header n
     	pcount = 0;
-    	while sib and sib.name == 'p': #get all ps that are adjacent to that h4
-    		print(sib)
+    	content = ""
+    	while sib and sib.name == 'p': # Get all <p>s that are adjacent to the current <h4>
+    		content = content + " " + sib.text # Append all <p>s together
     		sib = sib.find_next_sibling()
     		pcount+=1
-    	print(pcount)
-    	if pcount == 0:
-    		print("E M P T Y HEADER")	
+    	thisRow.append(content)
+
+    # print(thisRow)
+    print("Made it!")
+    return(thisRow)
 
 
 
